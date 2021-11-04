bca4644e5c1d001a38dc6c59fa822d977da4a405
nonistrati <93589021+nonistrati@users.noreply.github.com>
2021-11-04 14:30:45 +0200

    Update ch11.c

--- 1/ch11.c
+++ 1/ch11.c
@@ -1,15 +1,34 @@
 #include <stdio.h>
-#include <sys/mman.h>
-#include <stdlib.h>
-#include <sys/stat.h>
+#include <string.h>
 #include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <unistd.h>
-#include <sys/mman.h>
+#include <stdlib.h>
+#include <sys/wait.h>
 
-main(void){
-     if (msync(0x7ff04d1a5000, 4096,MS_SYNC )< 0 ) {
-          perror("msync failed with error:");
-          return -1;
-     }
-     else (void) printf("%s","msync completed successfully.");
+int main() {
+    int fd1;
+    int stat;
+    fd1 = open("secret_number", O_RDWR);
+    char str1[72];
+    int pid = fork();
+    if (pid<0) perror("error");
+    else if (pid == 0) {
+        printf("child %d\n",getpid());
+        char *const argv[] = {"./riddle",NULL};
+        int status = execv(argv[0], argv);
+        if (status < 0) {  //τρεχει μονο αν αποτυχει η execv
+	    perror("Error running executable");
+            exit(-1);
+        }
+    }
+    else {
+        printf("parent %d\n",getpid());
+        sleep(5);
+        printf("parent %d\n",getpid());
+        read(fd1, str1, 72);
+        printf("%s",str1);
+        if(sizeof(str1)==0) printf("no");
+    }
 }
