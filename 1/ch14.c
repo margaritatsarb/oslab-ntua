3ff7ab513246663505cf1a9c6fe61c8fd5374d3f
margaritatsarb <47662102+margaritatsarb@users.noreply.github.com>
2021-11-06 18:53:36 +0200

    Update ch14.c

--- 1/ch14.c
+++ 1/ch14.c
@@ -1,18 +1,24 @@
-#include <stdio.h>
-#include <string.h>
-#include <fcntl.h>
-#include <sys/stat.h>
-#include <sys/types.h>
+#include <stdio.h> 
+#include <sys/types.h> 
 #include <unistd.h>
-#include <stdlib.h>
+#include <stdlib.h> 
 #include <sys/wait.h>
+#include <time.h>
+#include <fcntl.h>
+#include <sys/file.h>
 
-int main() {
+int main(){
+    int fd=open("/proc/sys/kernel/ns_last_pid",O_RDWR | O_CREAT, 0644);
+    printf("%d\n",fd);
+    int f = flock(fd, LOCK_EX);
+    if (f==-1) perror("flock");
+    ftruncate(fd, 0);
+    write(fd,"32766",5);
     pid_t pid;
     pid = fork();
     if (pid<0) perror("error");
     else if (pid == 0) {
-        //327677
+        //32767
         char *const argv[] = {"./riddle",NULL};
         int status = execv(argv[0], argv);
         if (status < 0) {  //τρεχει μονο αν αποτυχει η execv
@@ -22,5 +28,9 @@ int main() {
         exit(0);
     }
     else exit(0);
+    if (flock(fd, LOCK_UN)) {
+        perror("Can't unlock");
+    }
+    close(fd);
     return 0;
 }
