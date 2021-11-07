dcf9cefdf7dd7de34a4b2174f3469d4d86877f4d
margaritatsarb <47662102+margaritatsarb@users.noreply.github.com>
2021-11-07 13:15:11 +0200

    Update ch12.c

--- 1/ch12.c
+++ 1/ch12.c
@@ -6,25 +6,33 @@
 #include <unistd.h>
 
 int main(){
-    char *const argv[] = {"./riddle",NULL};
-    int status = execv(argv[0], argv);
-    if (status < 0) {  //τρεχει μονο αν αποτυχει η execv
-	perror("Error running executable");
-        exit(-1);
+    pid_t pid;
+    pid = fork();
+    if (pid<0) perror("error");
+    else if (pid == 0) {
+        char *const argv[] = {"./riddle",NULL};
+        int status = execv(argv[0], argv);
+        if (status < 0) {  //τρεχει μονο αν αποτυχει η execv
+	    perror("Error running executable");
+            exit(-1);
+        }
     }
-    unsigned char *f;
-    struct stat statbuf;
-    //const char *filepath = argv[1];
-    int fd = open("secret_number", O_RDWR);
-    printf("%d\n",fd);
-    //int status = fstat (fd, & statbuf);
-    int size=statbuf.st_size;
-    f = (char *) mmap(NULL,4096,PROT_READ|PROT_WRITE,MAP_SHARED,4,0);
-    //close(fd);
-    for (int i = 0; i < size; i++) {
-        char c;
-        c = f[i];
-        putchar(c);
-    }
+    else {
+        char text[20];
+        gets(text);
+        int fd;
+        if ((fd=open(text, O_RDWR)) < 0){
+            perror("open");
+            return 0;
+        }
+        if ((lseek (fd, 111, SEEK_SET)) < 0){
+            perror("lseek");
+            return 0;
+        }
+        char textt[2];
+        fgets(textt, sizeof(textt), stdin);
+        write (fd, textt, 1);
+        mmap (0, 4096, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
     
+    }
 }
