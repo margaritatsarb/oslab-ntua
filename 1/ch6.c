155e52e88e982fd234d4409c85f26ede9eca0260
nonistrati <93589021+nonistrati@users.noreply.github.com>
2021-11-08 11:38:31 +0200

    Update ch6.c

--- 1/ch6.c
+++ 1/ch6.c
@@ -8,12 +8,14 @@
 
 int main(){
     int fd,a;
-    fd = open ("s.txt", O_RDWR);
-    dup2(fd, 33);
-    dup2(fd, 54);
-    dup2(fd, 34);
-    fd = open ("ss.txt", O_RDWR);
-    dup2(fd, 53);
+    int fd1[2];
+    int fd2[2];
+    pipe(fd1);
+    pipe(fd2);
+    dup2(fd1[0], 33);
+    dup2(fd1[1], 34);
+    dup2(fd2[0], 53);
+    dup2(fd2[1], 54);
     
     char *const argv[] = {"./riddle",NULL};
     int status = execv(argv[0], argv);
